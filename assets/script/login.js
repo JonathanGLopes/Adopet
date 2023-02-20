56ab0976a7f8bd5118d4b61395b962b39a4da933
Jonathan Lopes <jonathanfiles10@gmail.com>
2023-02-20 16:22:05 -0300

    Validando email e senha do login + modal de erro

--- assets/script/login.js
+++ assets/script/login.js
@@ -3,19 +3,33 @@ const $form = document.querySelector("form");
 $form.addEventListener("submit", (e) =>{
     e.preventDefault();
 
-    if(validaEmail && validaSenha){
-        validaEmail();
-        validaSenha();
-    }
-    
+    validaCampos();
+
 });
 
 
-function validaEmail(){
+function validaCampos(){
     const $inputEmail = document.querySelector(".form-input-email").value;
+    const $inputSenha = document.querySelector(".form-input-senha").value;
+    const $textoModalErro = document.querySelector(".modal-content-input");
 
-    if($inputEmail.length < 10){
-        return alert("Email incorreto, verifique e tente novamente");
+    if($inputEmail === '' && $inputSenha === ''){
+        $textoModalErro.textContent = "Preencha os campos.";
+        showModal();
+    } else {
+        validaEmail();
+        validaSenha();
+    }
+}
+
+
+function validaEmail(email){
+    const $inputEmail = document.querySelector(".form-input-email").value;
+    const $textoModalEmail = document.querySelector(".modal-content-input");
+
+    if($inputEmail === ''){
+        $textoModalEmail.textContent = "Por favor, informe o email";
+        showModal();
     }
 
 }
@@ -23,12 +37,37 @@ function validaEmail(){
 
 function validaSenha(){
     const $inputSenha = document.querySelector(".form-input-senha").value;
-    
-    if($inputSenha.length <= 7){
-        return alert("Senha incorreta, verifique e tente novamente");
+    const $textoModalSenha = document.querySelector(".modal-content-input");
+
+    if($inputSenha === ''){
+        $textoModalSenha.textContent = "Por favor, informe a senha";
+        showModal();
+    }
+    else if($inputSenha.length < 8){
+        $textoModalSenha.textContent = "Senha inválida!";
+        showModal();
     } else {
         window.location.href = "/assets/pages/pets.html";
     }
-
     
 }
+
+function showModal(){
+    const $modal = document.querySelector(".modal-erro");
+    const $closeModalBtn = document.querySelector(".closeModalBtn");
+
+    $closeModalBtn.addEventListener('click', function() {
+        $modal.style.display = 'none';
+      });
+
+    $modal.style.display = "flex";
+}
+
+function closeModal(){
+    const $modal = document.querySelector(".modal-erro");
+
+    $modal.style.display = "none";
+
+}
+
+const intervalo = setInterval(closeModal, 3000);
